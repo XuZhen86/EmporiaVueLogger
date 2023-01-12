3105eb1ec83c18560de42415b3ede504b285b6e7
Zhen Xu <curt_xu@me.com>
2023-01-11 23:37:41 -0500

    Call mqtt_client.subscribe() in mqtt_client_on_connect().
    mqtt_client_on_connect() is needed to ensure topics are re-subscribed after reconnection.

--- emporia_vue_logger/collectrecords.py
+++ emporia_vue_logger/collectrecords.py
@@ -45,6 +45,16 @@ _MQTT_TOPICS = flags.DEFINE_multi_string(
 )
 
 
+def mqtt_client_on_connect(
+    mqtt_client: Client,
+    userdata: Any,
+    flags: dict[str, Any],
+    rc: int,
+) -> None:
+  for topic in _MQTT_TOPICS.value:
+    mqtt_client.subscribe(topic)
+
+
 def mqtt_client_on_message(
     mqtt_client: Client,
     userdata: Any,
@@ -61,22 +71,19 @@ def mqtt_client_on_message(
       EmporiaVueLog.from_mqtt_payload(time.time_ns(), mqtt_payload).to_influxdb_line_protocols())
 
 
-def get_mqtt_client(on_message) -> Client:
-  mqtt_client = Client(client_id=_MQTT_CLIENT_ID.value)
-  mqtt_client.username_pw_set(username=_MQTT_USERNAME.value, password=_MQTT_PASSWORD.value)
-  mqtt_client.connect(host=_MQTT_BROKER_ADDRESS.value, port=_MQTT_BROKER_PORT.value, keepalive=10)
-  for topic in _MQTT_TOPICS.value:
-    mqtt_client.subscribe(topic)
-  mqtt_client.on_message = on_message
-  return mqtt_client
-
-
 def collect_records(args: list[str]) -> None:
   with LineProtocolCacheProducer() as producer:
-    on_message = lambda mc, u, mm: mqtt_client_on_message(mc, u, mm, producer)
-    mqtt_client = get_mqtt_client(on_message)
+    mqtt_client = Client(client_id=_MQTT_CLIENT_ID.value, reconnect_on_failure=True)
+    mqtt_client.on_connect = mqtt_client_on_connect
+    mqtt_client.on_message = lambda mc, u, mm: mqtt_client_on_message(mc, u, mm, producer)
+    mqtt_client.username_pw_set(username=_MQTT_USERNAME.value, password=_MQTT_PASSWORD.value)
 
     try:
+      mqtt_client.connect(
+          host=_MQTT_BROKER_ADDRESS.value,
+          port=_MQTT_BROKER_PORT.value,
+          keepalive=10,
+      )
       mqtt_client.loop_forever()
     finally:
       mqtt_client.disconnect()
